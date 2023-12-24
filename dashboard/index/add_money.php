9dab01cd7fd14a1bff8fd45e042089123f71507a
Sir Abdulkarim Hussain SITD <abdulkarimhussain7@gmail.com>
2023-12-24 07:44:23 +0100

    add add_money

--- dashboard/index/add_money.php
+++ dashboard/index/add_money.php
@@ -26,11 +26,7 @@ try {
     $customer_email = $user_email; // Replace with actual email
     $tx_ref = 'Funds_' . uniqid(); // Replace with actual reference
 
-    // Concatenate values for hashing
-    $string_to_be_hashed = $fullAmount . $currency . $customer_email . $tx_ref . $secret_key;
-
-    // Generate payload hash
-    $payload_hash = hash('sha256', $string_to_be_hashed);
+   
 
     $public_key = "FLWPUBK-7629f619c8c46d8a65020bb53f1def79-X";
     $currency = "NGN";
@@ -45,7 +41,7 @@ try {
         "logo" => $logo,
     );
 
-   
+
 
     $meta = array(
         "name" => $user_name,
@@ -54,6 +50,13 @@ try {
     $customer = array(
         "email" => $user_email,
     );
+
+     // Concatenate values for hashing
+     $string_to_be_hashed = $fullAmount . $currency . $customer_email . $tx_ref . $secret_key;
+
+     // Generate payload hash
+     $payload_hash = hash('sha256', $string_to_be_hashed);
+
 
     $paymentPayload = array(
         "public_key" => $public_key,
@@ -69,7 +72,7 @@ try {
     );
 
     $paymentPayloadJson = json_encode($paymentPayload);
-header('Content-Type: application/json');
+    header('Content-Type: application/json');
 
 
     $type = "Bank";
@@ -86,7 +89,8 @@ header('Content-Type: application/json');
 
 
 
-echo trim($paymentPayloadJson);
+
+    echo trim($paymentPayloadJson);
 
 
 } catch (Exception $e) {
