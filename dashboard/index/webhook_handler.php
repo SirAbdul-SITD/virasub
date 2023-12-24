b3998cd28b81442c82ec33b3fadc57054136bdad
Sir Abdulkarim Hussain SITD <abdulkarimhussain7@gmail.com>
2023-12-24 02:54:29 +0100

    logo, webhook

--- dashboard/index/webhook_handler.php
+++ dashboard/index/webhook_handler.php
@@ -1,79 +1,121 @@
 <?php
 require("settings.php");
 
-// Secret key for validating the webhook request (set this to your secret key)
-$webhookSecret = "your_webhook_secret_key";
+// Specify your secret hash (recommended to be stored as an environment variable)
+$secretHash = "987d4s6ivujychtgrxsecdtfvybgn98765e4wno9vy5"; // Replace with your actual secret hash
 
-// Read the incoming webhook data
-$input = file_get_contents('php://input');
-$hash = $_SERVER['HTTP_VERIF_HASH'];
+// Accept the JSON data from the webhook
+$jsonData = file_get_contents('php://input');
+$data = json_decode($jsonData, true);
 
-// Verify the webhook request
-if ($hash === hash_hmac("sha512", $input, $webhookSecret)) {
-    $data = json_decode($input, true);
+if ($data) {
+    // Verify the secret hash
+    $webhookSignature = $_SERVER['HTTP_VERIF-HASH'];
+    if (!$webhookSignature || $webhookSignature !== $secretHash) {
+        // Signature doesn't match; discard the request
+        http_response_code(401); // Unauthorized
+        echo "Unauthorized request.";
+        exit;
+    }
 
-    // Extract relevant data from the webhook payload
-    // Extract specific data fields
+
+
+
+    // Extract data
     $event = $data['event'];
-    $txRef = $data['data']['tx_ref'];
-    $flwRef = $data['data']['flw_ref'];
+    $tx_ref = $data['data']['tx_ref'];
+    $flw_ref = $data['data']['flw_ref'];
+    $device_fingerprint = $data['data']['device_fingerprint'];
     $amount = $data['data']['amount'];
     $currency = $data['data']['currency'];
+    $charged_amount = $data['data']['charged_amount'];
+    $app_fee = $data['data']['app_fee'];
+    $merchant_fee = $data['data']['merchant_fee'];
+    $processor_response = $data['data']['processor_response'];
+    $auth_model = $data['data']['auth_model'];
+    $ip = $data['data']['ip'];
+    $narration = $data['data']['narration'];
     $status = $data['data']['status'];
-    $paymentType = $data['data']['payment_type'];
-    $createdAt = $data['data']['created_at'];
+    $payment_type = $data['data']['payment_type'];
+    $created_at = $data['data']['created_at'];
+    $account_id = $data['data']['account_id'];
 
-    $customerName = $data['data']['customer']['name'];
-    $customerPhoneNumber = $data['data']['customer']['phone_number'];
-    $customerEmail = $data['data']['customer']['email'];
+    // Check if customer data is present
+    if (isset($data['data']['customer'])) {
+        $customer = $data['data']['customer'];
+        $customer_id = $customer['id'];
+        $customer_name = $customer['name'];
+        $customer_phone_number = $customer['phone_number'];
+        $customer_email = $customer['email'];
+        $customer_created_at = $customer['created_at'];
+    } else {
+        $customer_id = $customer_name = $customer_phone_number = $customer_email = $customer_created_at = null;
+    }
 
-    $cardType = $data['data']['card']['type'];
-    $cardIssuer = $data['data']['card']['issuer'];
-    $cardExpiry = $data['data']['card']['expiry'];
+    // Insert data into the database
+    $stmt = $pdo->prepare("INSERT INTO webhook_data 
+    (event, tx_ref, flw_ref, device_fingerprint, amount, currency, charged_amount, app_fee, merchant_fee, processor_response, auth_model, ip, narration, status, payment_type, created_at, account_id, customer_id, customer_name, customer_phone_number, customer_email, customer_created_at) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-    // Now you can use these extracted data fields as needed for your application
-// For example, you can update the user's balance in your database based on the transaction status
+    $stmt->execute([
+        $event, $tx_ref, $flw_ref, $device_fingerprint, $amount, $currency, $charged_amount, $app_fee, $merchant_fee, $processor_response, $auth_model, $ip, $narration, $status, $payment_type, $created_at, $account_id, $customer_id, $customer_name, $customer_phone_number, $customer_email, $customer_created_at
+    ]);
 
-    // Respond to the webhook to acknowledge receipt
-    http_response_code(200);
 
-    if ($status === 'successful') {
-        // Perform database update to add the amount to user's balance
-        $newBal = $balance + $amount;
-        // Your database update logic here
+    $query = "SELECT `status` FROM funding WHERE trx_ref = :trx_ref";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':trx_ref', $tx_ref, PDO::PARAM_STR);
+    $stmt->execute();
+    $transactionData = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        $type = "Card";
+
+    if ($transactionData['status'] !== $status || $status == "successful") {
+        
+        $trx_status = "Completed";
 
         $updateQuery = "UPDATE funding SET status = :status, type = :type WHERE trx_ref = :trx_ref";
         $updateStmt = $pdo->prepare($updateQuery);
-        $updateStmt->bindParam(':status', $status, PDO::PARAM_STR);
-        $updateStmt->bindParam(':trx_ref', $txRef, PDO::PARAM_STR);
-        $updateStmt->bindParam(':type', $type, PDO::PARAM_STR);
+        $updateStmt->bindParam(':status', $trx_status, PDO::PARAM_STR);
+        $updateStmt->bindParam(':trx_ref', $tx_ref, PDO::PARAM_STR);
+        $updateStmt->bindParam(':type', $payment_type, PDO::PARAM_STR);
         $updateStmt->execute();
 
-        $updateQuery = "UPDATE users SET balance = :newBal WHERE email = :user_email";
+
+        $newBal = $balance + $amount;
+
+        $updateQuery = "UPDATE users SET balance = :newBal WHERE user = :user_email";
         $updateStmt = $pdo->prepare($updateQuery);
         $updateStmt->bindParam(':newBal', $newBal, PDO::PARAM_STR);
-        $updateStmt->bindParam(':user_email', $user_email, PDO::PARAM_STR);
+        $updateStmt->bindParam(':user_email', $customer_email, PDO::PARAM_STR);
         $updateStmt->execute();
 
 
-        // Respond with a success status to Flutterwave
-        http_response_code(200);
-    } else {
-        // Respond with a status indicating unsuccessful processing
-        http_response_code(400);
+    } elseif ($status == "failed") {
 
+        $trx_status = "Failed";
+        
         $updateQuery = "UPDATE funding SET status = :status, type = :type WHERE trx_ref = :trx_ref";
         $updateStmt = $pdo->prepare($updateQuery);
-        $updateStmt->bindParam(':status', $status, PDO::PARAM_STR);
-        $updateStmt->bindParam(':trx_ref', $txRef, PDO::PARAM_STR);
-        $updateStmt->bindParam(':type', $type, PDO::PARAM_STR);
+        $updateStmt->bindParam(':status', $trx_status, PDO::PARAM_STR);
+        $updateStmt->bindParam(':trx_ref', $tx_ref, PDO::PARAM_STR);
+        $updateStmt->bindParam(':type', $payment_type, PDO::PARAM_STR);
         $updateStmt->execute();
+
     }
+
+    // Close the database connection
+    $pdo = null;
+
+
+
+
+    // Respond with a 200 status code to acknowledge receipt
+    http_response_code(200);
+    echo "Webhook data received and processed successfully.";
 } else {
-    // Respond with a status indicating invalid request
-    http_response_code(403);
+    // Respond with an error if the data is not valid JSON
+    http_response_code(400); // Bad Request
+    echo "Invalid JSON data.";
 }
 
 ?>
