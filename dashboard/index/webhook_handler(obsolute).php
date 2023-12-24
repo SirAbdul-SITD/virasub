f1712f282c16effecb33d2a937e556385b7ab511
Sir Abdulkarim Hussain SITD <abdulkarimhussain7@gmail.com>
2023-12-24 09:50:53 +0100

    webhook_handler

---
+++ dashboard/index/webhook_handler(obsolute).php
@@ -0,0 +1,121 @@
+<?php
+require("settings.php");
+
+// Specify your secret hash (recommended to be stored as an environment variable)
+$secretHash = "987d4s6ivujychtgrxsecdtfvybgn98765e4wno9vy5"; // Replace with your actual secret hash
+
+// Accept the JSON data from the webhook
+$jsonData = file_get_contents('php://input');
+$data = json_decode($jsonData, true);
+
+if ($data) {
+    // Verify the secret hash
+    $webhookSignature = $_SERVER['HTTP_VERIF-HASH'];
+    if (!$webhookSignature || $webhookSignature !== $secretHash) {
+        // Signature doesn't match; discard the request
+        http_response_code(401); // Unauthorized
+        echo "Unauthorized request.";
+        exit;
+    }
+
+
+
+
+    // Extract data
+    $event = $data['event'];
+    $tx_ref = $data['data']['tx_ref'];
+    $flw_ref = $data['data']['flw_ref'];
+    $device_fingerprint = $data['data']['device_fingerprint'];
+    $amount = $data['data']['amount'];
+    $currency = $data['data']['currency'];
+    $charged_amount = $data['data']['charged_amount'];
+    $app_fee = $data['data']['app_fee'];
+    $merchant_fee = $data['data']['merchant_fee'];
+    $processor_response = $data['data']['processor_response'];
+    $auth_model = $data['data']['auth_model'];
+    $ip = $data['data']['ip'];
+    $narration = $data['data']['narration'];
+    $status = $data['data']['status'];
+    $payment_type = $data['data']['payment_type'];
+    $created_at = $data['data']['created_at'];
+    $account_id = $data['data']['account_id'];
+
+    // Check if customer data is present
+    if (isset($data['data']['customer'])) {
+        $customer = $data['data']['customer'];
+        $customer_id = $customer['id'];
+        $customer_name = $customer['name'];
+        $customer_phone_number = $customer['phone_number'];
+        $customer_email = $customer['email'];
+        $customer_created_at = $customer['created_at'];
+    } else {
+        $customer_id = $customer_name = $customer_phone_number = $customer_email = $customer_created_at = null;
+    }
+
+    // Insert data into the database
+    $stmt = $pdo->prepare("INSERT INTO webhook_data 
+    (event, tx_ref, flw_ref, device_fingerprint, amount, currency, charged_amount, app_fee, merchant_fee, processor_response, auth_model, ip, narration, status, payment_type, created_at, account_id, customer_id, customer_name, customer_phone_number, customer_email, customer_created_at) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+    $stmt->execute([
+        $event, $tx_ref, $flw_ref, $device_fingerprint, $amount, $currency, $charged_amount, $app_fee, $merchant_fee, $processor_response, $auth_model, $ip, $narration, $status, $payment_type, $created_at, $account_id, $customer_id, $customer_name, $customer_phone_number, $customer_email, $customer_created_at
+    ]);
+
+
+    $query = "SELECT `status` FROM funding WHERE trx_ref = :trx_ref";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':trx_ref', $tx_ref, PDO::PARAM_STR);
+    $stmt->execute();
+    $transactionData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    if ($transactionData['status'] !== $status || $status == "successful") {
+        
+        $trx_status = "Completed";
+
+        $updateQuery = "UPDATE funding SET status = :status, type = :type WHERE trx_ref = :trx_ref";
+        $updateStmt = $pdo->prepare($updateQuery);
+        $updateStmt->bindParam(':status', $trx_status, PDO::PARAM_STR);
+        $updateStmt->bindParam(':trx_ref', $tx_ref, PDO::PARAM_STR);
+        $updateStmt->bindParam(':type', $payment_type, PDO::PARAM_STR);
+        $updateStmt->execute();
+
+
+        $newBal = $balance + $amount;
+
+        $updateQuery = "UPDATE users SET balance = :newBal WHERE user = :user_email";
+        $updateStmt = $pdo->prepare($updateQuery);
+        $updateStmt->bindParam(':newBal', $newBal, PDO::PARAM_STR);
+        $updateStmt->bindParam(':user_email', $customer_email, PDO::PARAM_STR);
+        $updateStmt->execute();
+
+
+    } elseif ($status == "failed") {
+
+        $trx_status = "Failed";
+        
+        $updateQuery = "UPDATE funding SET status = :status, type = :type WHERE trx_ref = :trx_ref";
+        $updateStmt = $pdo->prepare($updateQuery);
+        $updateStmt->bindParam(':status', $trx_status, PDO::PARAM_STR);
+        $updateStmt->bindParam(':trx_ref', $tx_ref, PDO::PARAM_STR);
+        $updateStmt->bindParam(':type', $payment_type, PDO::PARAM_STR);
+        $updateStmt->execute();
+
+    }
+
+    // Close the database connection
+    $pdo = null;
+
+
+
+
+    // Respond with a 200 status code to acknowledge receipt
+    http_response_code(200);
+    echo "Webhook data received and processed successfully.";
+} else {
+    // Respond with an error if the data is not valid JSON
+    http_response_code(400); // Bad Request
+    echo "Invalid JSON data.";
+}
+
+?>
--- dashboard/index/webhook_handler.php
+++ dashboard/index/webhook_handler.php
@@ -1,65 +1,80 @@
 <?php
+
 require("settings.php");
 
-// Specify your secret hash (recommended to be stored as an environment variable)
-$secretHash = "987d4s6ivujychtgrxsecdtfvybgn98765e4wno9vy5"; // Replace with your actual secret hash
 
-// Accept the JSON data from the webhook
-$jsonData = file_get_contents('php://input');
-$data = json_decode($jsonData, true);
+// Get the raw POST data
+$webhookData = file_get_contents("php://input");
 
-if ($data) {
-    // Verify the secret hash
-    $webhookSignature = $_SERVER['HTTP_VERIF-HASH'];
-    if (!$webhookSignature || $webhookSignature !== $secretHash) {
-        // Signature doesn't match; discard the request
-        http_response_code(401); // Unauthorized
-        echo "Unauthorized request.";
-        exit;
+// Decode the JSON data
+$decodedData = json_decode($webhookData, true);
+
+// Check if decoding was successful
+if ($decodedData !== null) {
+    $status = $decodedData['data']['status'];
+    $tx_ref = $decodedData['data']['tx_ref'];
+    $payment_type = $decodedData['data']['payment_type'];
+    $customer_email = $decodedData['data']['customer']['email'];
+    $amount = $decodedData['data']['amount'];
+
+
+    // check if this webhook already exist
+    $query = "SELECT `tx_ref` FROM webhook_data WHERE tx_ref = :tx_ref";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':tx_ref', $tx_ref, PDO::PARAM_STR);
+    $stmt->execute();
+    $transactionData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+    // dump into webhook table
+    if ($transactionData == null) {
+
+        // Prepare and execute the SQL query
+        $insertQuery = "INSERT INTO `webhook_data` (
+        `event_status`, `tx_ref`, `flw_ref`, `device_fingerprint`, `amount`, 
+        `currency`, `charged_amount`, `app_fee`, `merchant_fee`, `processor_response`, 
+        `auth_model`, `ip`, `narration`, `payment_status`, `payment_type`, 
+        `created_at`, `account_id`, `customer_id`, `customer_name`, 
+        `customer_phone`, `customer_email`
+    ) VALUES (
+        :eventStatus, :txRef, :flwRef, :deviceFingerprint, :amount, 
+        :currency, :chargedAmount, :appFee, :merchantFee, :processorResponse, 
+        :authModel, :ip, :narration, :status, :paymentType, 
+        :createdAt, :accountId, :customerId, :customerName, 
+        :customerPhone, :customerEmail
+    )";
+
+        $insertStmt = $pdo->prepare($insertQuery);
+
+        // Bind parameters
+        $insertStmt->bindParam(':eventStatus', $data['data']['status'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':txRef', $data['data']['tx_ref'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':flwRef', $data['data']['flw_ref'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':deviceFingerprint', $data['data']['device_fingerprint'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':amount', $data['data']['amount'], PDO::PARAM_INT);
+        $insertStmt->bindParam(':currency', $data['data']['currency'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':chargedAmount', $data['data']['charged_amount'], PDO::PARAM_INT);
+        $insertStmt->bindParam(':appFee', $data['data']['app_fee'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':merchantFee', $data['data']['merchant_fee'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':processorResponse', $data['data']['processor_response'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':authModel', $data['data']['auth_model'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':ip', $data['data']['ip'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':narration', $data['data']['narration'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':status', $data['data']['status'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':paymentType', $data['data']['payment_type'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':createdAt', $data['data']['created_at'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':accountId', $data['data']['account_id'], PDO::PARAM_INT);
+        $insertStmt->bindParam(':customerId', $data['data']['customer']['id'], PDO::PARAM_INT);
+        $insertStmt->bindParam(':customerName', $data['data']['customer']['name'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':customerPhone', $data['data']['customer']['phone_number'], PDO::PARAM_STR);
+        $insertStmt->bindParam(':customerEmail', $data['data']['customer']['email'], PDO::PARAM_STR);
+
+        // Execute the query
+        $insertStmt->execute();
     }
 
 
 
-
-    // Extract data
-    $event = $data['event'];
-    $tx_ref = $data['data']['tx_ref'];
-    $flw_ref = $data['data']['flw_ref'];
-    $device_fingerprint = $data['data']['device_fingerprint'];
-    $amount = $data['data']['amount'];
-    $currency = $data['data']['currency'];
-    $charged_amount = $data['data']['charged_amount'];
-    $app_fee = $data['data']['app_fee'];
-    $merchant_fee = $data['data']['merchant_fee'];
-    $processor_response = $data['data']['processor_response'];
-    $auth_model = $data['data']['auth_model'];
-    $ip = $data['data']['ip'];
-    $narration = $data['data']['narration'];
-    $status = $data['data']['status'];
-    $payment_type = $data['data']['payment_type'];
-    $created_at = $data['data']['created_at'];
-    $account_id = $data['data']['account_id'];
-
-    // Check if customer data is present
-    if (isset($data['data']['customer'])) {
-        $customer = $data['data']['customer'];
-        $customer_id = $customer['id'];
-        $customer_name = $customer['name'];
-        $customer_phone_number = $customer['phone_number'];
-        $customer_email = $customer['email'];
-        $customer_created_at = $customer['created_at'];
-    } else {
-        $customer_id = $customer_name = $customer_phone_number = $customer_email = $customer_created_at = null;
-    }
-
-    // Insert data into the database
-    $stmt = $pdo->prepare("INSERT INTO webhook_data 
-    (event, tx_ref, flw_ref, device_fingerprint, amount, currency, charged_amount, app_fee, merchant_fee, processor_response, auth_model, ip, narration, status, payment_type, created_at, account_id, customer_id, customer_name, customer_phone_number, customer_email, customer_created_at) 
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-
-    $stmt->execute([
-        $event, $tx_ref, $flw_ref, $device_fingerprint, $amount, $currency, $charged_amount, $app_fee, $merchant_fee, $processor_response, $auth_model, $ip, $narration, $status, $payment_type, $created_at, $account_id, $customer_id, $customer_name, $customer_phone_number, $customer_email, $customer_created_at
-    ]);
 
 
     $query = "SELECT `status` FROM funding WHERE trx_ref = :trx_ref";
@@ -68,9 +83,8 @@ if ($data) {
     $stmt->execute();
     $transactionData = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
     if ($transactionData['status'] !== $status || $status == "successful") {
-        
+
         $trx_status = "Completed";
 
         $updateQuery = "UPDATE funding SET status = :status, type = :type WHERE trx_ref = :trx_ref";
@@ -93,7 +107,7 @@ if ($data) {
     } elseif ($status == "failed") {
 
         $trx_status = "Failed";
-        
+
         $updateQuery = "UPDATE funding SET status = :status, type = :type WHERE trx_ref = :trx_ref";
         $updateStmt = $pdo->prepare($updateQuery);
         $updateStmt->bindParam(':status', $trx_status, PDO::PARAM_STR);
@@ -105,7 +119,6 @@ if ($data) {
 
     // Close the database connection
     $pdo = null;
-
 
 
 
