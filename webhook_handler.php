6856db457968d25bbf99019551507ae34004b8cb
Sir Abdulkarim Hussain SITD <abdulkarimhussain7@gmail.com>
2024-01-02 00:28:38 +0100

    webhook_handler

--- webhook_handler.php
+++ webhook_handler.php
@@ -4,7 +4,7 @@ require("settings.php");
 
 
 // Get the raw POST data
-$webhookData = file_get_contents("php://input");
+$webhookData = '{"event":"charge.completed","data":{"id":4820873,"tx_ref":"Funds_6592e1b7cbfe5","flw_ref":"flwm3s4m0c1704124874181","device_fingerprint":"0c70c48bf1e4486fd31fb467f152596a","amount":1030,"currency":"NGN","charged_amount":1030,"app_fee":14.42,"merchant_fee":0,"processor_response":"Transaction completed","auth_model":"USSD","ip":"52.209.154.143","narration":"Shinami Information Technology Development ","status":"successful","payment_type":"ussd","created_at":"2024-01-01T16:01:13.000Z","account_id":161061,"customer":{"id":2317751,"name":"Anonymous Customer","phone_number":null,"email":"abdulkarimhussain2@gmail.com","created_at":"2024-01-01T16:01:13.000Z"}},"event.type":"USSD_TRANSACTION"}';
 
 // Decode the JSON data
 $decodedData = json_decode($webhookData, true);
