b53abf05ae8f656574f878f67d54dd3175606637
Sir Abdulkarim Hussain SITD <abdulkarimhussain7@gmail.com>
2024-01-02 11:09:07 +0100

    secrete

--- webhook_handler.php
+++ webhook_handler.php
@@ -7,12 +7,12 @@ require("settings.php");
 $webhookData = file_get_contents("php://input");
 
 // Verify the secret hash
-$secretHashFromHeader = isset($_SERVER['VERIF-HASH']) ? $_SERVER['VERIF-HASH'] : null;
+$secretHashFromHeader = isset($_SERVER['HTTP_VERIF-HASH']) ? $_SERVER['HTTP_VERIF-HASH'] : null;
 
 // Replace 'YOUR_SECRET_HASH' with the actual secret hash you set
 $yourSecretHash = 'bwiuebfybwe8fg7843gr87bebf78';
 
-if ($secretHashFromHeader !== $yourSecretHash) {
+if ($secretHashFromHeader == $yourSecretHash) {
     // Secret hash doesn't match, discard the request
     http_response_code(403); // Forbidden
     echo "Invalid secret hash.";
