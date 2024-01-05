82261276f9f7a04debd2644e2f1c33fac06c320e
Sir Abdulkarim Hussain SITD <abdulkarimhussain7@gmail.com>
2024-01-05 18:01:09 +0100

    price list

--- dashboard/index/settings.php
+++ dashboard/index/settings.php
@@ -78,31 +78,24 @@ if ($row) {
 
     $plansData = array(
       [6, '1', 'SME', '500MB', '₦145.00', '1 Month'],
+      [83, '1', 'GIFTING', '1.0GB', '₦250.00', '7 days'],
       [1, '1', 'SME', '1.0GB', '₦290.00', '1 Month'],
       [3, '1', 'SME', '2.0GB', '₦570.00', '1 Month'],
-      [2, '1', 'SME', '3.0GB', '₦850.00', '1 Month'],
+      [85, '1', 'GIFTING', '3.0GB', '₦800.00', '1 Month'],
       [4, '1', 'SME', '5.0GB', '₦1,400.00', '1 Month'],
       [5, '1', 'SME', '10.0GB', '₦2,750.00', '1 Month'],
-      
-      [7, '1', 'COOPERATE GIFTING', '500MB', '₦150.00', '1 Month'],
-      [8, '1', 'COOPERATE GIFTING', '1.0GB', '₦285.00', '1 Month'],
-      [9, '1', 'COOPERATE GIFTING', '2.0GB', '₦560.00', '1 Month'],
-      [10, '1', 'COOPERATE GIFTING', '3.0GB', '₦840.00', '1 Month'],
-      [11, '1', 'COOPERATE GIFTING', '5.0GB', '₦1,370.00', '1 Month'],
-      [12, '1', 'COOPERATE GIFTING', '10.0GB', '₦2,720.00', '1 Month'],
+      [51, '1', 'COOPERATE GIFTING', '15.0GB', '₦3,850.00', '30 days'],
 
+      [37, '2', 'COOPERATE GIFTING', '100MB', '₦60.00', '1 Month'],
+      [36, '2', 'COOPERATE GIFTING', '300MB', '₦100.00', '1 Month'],
       [13, '2', 'COOPERATE GIFTING', '500MB', '₦130.00', '1 Month'],
       [14, '2', 'COOPERATE GIFTING', '1.0GB', '₦240.00', '1 Month'],
       [15, '2', 'COOPERATE GIFTING', '2.0GB', '₦450.00', '1 Month'],
       [17, '2', 'COOPERATE GIFTING', '5.0GB', '₦1,070.00', '1 Month'],
       [18, '2', 'COOPERATE GIFTING', '10.0GB', '₦2,130.00', '1 Month'],
-      [36, '2', 'COOPERATE GIFTING', '300MB', '₦100.00', '1 Month'],
-      [37, '2', 'COOPERATE GIFTING', '100MB', '₦60.00', '1 Month'],
+      [62, '2', 'GIFTING', '20.0GB', '₦4,790.00', '30 days'],
 
-      [39, '3', 'GIFTING', '29.5GB', '₦7,500.00', '1 Month'],
-      [40, '3', 'GIFTING', '50.0GB', '₦9,500.00', '1 Month'],
-
-      [43, '4', 'GIFTING', '40.0GB', '₦8,500.00', '1 Month'],
+      
 
       [44, '3', 'COOPERATE GIFTING', '200MB', '₦105.00', '30 days'],
       [45, '3', 'COOPERATE GIFTING', '500MB', '₦140.00', '30 days'],
@@ -111,24 +104,15 @@ if ($row) {
       [48, '3', 'COOPERATE GIFTING', '3.0GB', '₦730.00', '30 days'],
       [49, '3', 'COOPERATE GIFTING', '5.0GB', '₦1,195.00', '30 days'],
       [50, '3', 'COOPERATE GIFTING', '10.0GB', '₦2,390.00', '30 days'],
-
-      [51, '1', 'COOPERATE GIFTING', '15.0GB', '₦3,850.00', '30 days'],
-
-      [53, '2', 'COOPERATE GIFTING', '20.0GB', '₦5,000.00', '30 days'],
-
-      [59, '1', 'GIFTING', '6.0GB', '₦2,420.00', '30 days'],
-      [60, '1', 'GIFTING', '10.0GB', '₦2,900.00', '30 days'],
-
-      [62, '2', 'GIFTING', '20.0GB', '₦5,000.00', '30 days'],
+      [39, '3', 'GIFTING', '29.5GB', '₦7,500.00', '1 Month'],
+      [40, '3', 'GIFTING', '50.0GB', '₦9,500.00', '1 Month'],
 
       [69, '4', 'COOPERATE GIFTING', '500MB', '₦140.00', '14 Days'],
       [70, '4', 'COOPERATE GIFTING', '1.0GB', '₦230.00', '30 days'],
       [71, '4', 'COOPERATE GIFTING', '2.0GB', '₦440.00', '30 days'],
       [72, '4', 'COOPERATE GIFTING', '3.0GB', '₦650.00', '30 days'],
       [73, '4', 'COOPERATE GIFTING', '5.0GB', '₦1,100.00', '30 days'],
-
-      [83, '1', 'GIFTING', '1.0GB', '₦275.00', '7 days'],
-      [85, '1', 'GIFTING', '3.0GB', '₦800.00', '1 Month'],
+      [43, '4', 'GIFTING', '40.0GB', '₦8,500.00', '1 Month'],
     );
     ?>
 
@@ -136,31 +120,24 @@ if ($row) {
     <script>
       plansData = [
         [6, '1', 'SME', '500MB', '₦145.00', '1 Month'],
+      [83, '1', 'GIFTING', '1.0GB', '₦250.00', '7 days'],
       [1, '1', 'SME', '1.0GB', '₦290.00', '1 Month'],
       [3, '1', 'SME', '2.0GB', '₦570.00', '1 Month'],
-      [2, '1', 'SME', '3.0GB', '₦850.00', '1 Month'],
+      [85, '1', 'GIFTING', '3.0GB', '₦800.00', '1 Month'],
       [4, '1', 'SME', '5.0GB', '₦1,400.00', '1 Month'],
       [5, '1', 'SME', '10.0GB', '₦2,750.00', '1 Month'],
-      
-      [7, '1', 'COOPERATE GIFTING', '500MB', '₦150.00', '1 Month'],
-      [8, '1', 'COOPERATE GIFTING', '1.0GB', '₦285.00', '1 Month'],
-      [9, '1', 'COOPERATE GIFTING', '2.0GB', '₦560.00', '1 Month'],
-      [10, '1', 'COOPERATE GIFTING', '3.0GB', '₦840.00', '1 Month'],
-      [11, '1', 'COOPERATE GIFTING', '5.0GB', '₦1,370.00', '1 Month'],
-      [12, '1', 'COOPERATE GIFTING', '10.0GB', '₦2,720.00', '1 Month'],
+      [51, '1', 'COOPERATE GIFTING', '15.0GB', '₦3,850.00', '30 days'],
 
+      [37, '2', 'COOPERATE GIFTING', '100MB', '₦60.00', '1 Month'],
+      [36, '2', 'COOPERATE GIFTING', '300MB', '₦100.00', '1 Month'],
       [13, '2', 'COOPERATE GIFTING', '500MB', '₦130.00', '1 Month'],
       [14, '2', 'COOPERATE GIFTING', '1.0GB', '₦240.00', '1 Month'],
       [15, '2', 'COOPERATE GIFTING', '2.0GB', '₦450.00', '1 Month'],
       [17, '2', 'COOPERATE GIFTING', '5.0GB', '₦1,070.00', '1 Month'],
       [18, '2', 'COOPERATE GIFTING', '10.0GB', '₦2,130.00', '1 Month'],
-      [36, '2', 'COOPERATE GIFTING', '300MB', '₦100.00', '1 Month'],
-      [37, '2', 'COOPERATE GIFTING', '100MB', '₦60.00', '1 Month'],
+      [62, '2', 'GIFTING', '20.0GB', '₦4,790.00', '30 days'],
 
-      [39, '3', 'GIFTING', '29.5GB', '₦7,500.00', '1 Month'],
-      [40, '3', 'GIFTING', '50.0GB', '₦9,500.00', '1 Month'],
-
-      [43, '4', 'GIFTING', '40.0GB', '₦8,500.00', '1 Month'],
+      
 
       [44, '3', 'COOPERATE GIFTING', '200MB', '₦105.00', '30 days'],
       [45, '3', 'COOPERATE GIFTING', '500MB', '₦140.00', '30 days'],
@@ -169,24 +146,15 @@ if ($row) {
       [48, '3', 'COOPERATE GIFTING', '3.0GB', '₦730.00', '30 days'],
       [49, '3', 'COOPERATE GIFTING', '5.0GB', '₦1,195.00', '30 days'],
       [50, '3', 'COOPERATE GIFTING', '10.0GB', '₦2,390.00', '30 days'],
-
-      [51, '1', 'COOPERATE GIFTING', '15.0GB', '₦3,850.00', '30 days'],
-
-      [53, '2', 'COOPERATE GIFTING', '20.0GB', '₦5,000.00', '30 days'],
-
-      [59, '1', 'GIFTING', '6.0GB', '₦2,420.00', '30 days'],
-      [60, '1', 'GIFTING', '10.0GB', '₦2,900.00', '30 days'],
-
-      [62, '2', 'GIFTING', '20.0GB', '₦5,000.00', '30 days'],
+      [39, '3', 'GIFTING', '29.5GB', '₦7,500.00', '1 Month'],
+      [40, '3', 'GIFTING', '50.0GB', '₦9,500.00', '1 Month'],
 
       [69, '4', 'COOPERATE GIFTING', '500MB', '₦140.00', '14 Days'],
       [70, '4', 'COOPERATE GIFTING', '1.0GB', '₦230.00', '30 days'],
       [71, '4', 'COOPERATE GIFTING', '2.0GB', '₦440.00', '30 days'],
       [72, '4', 'COOPERATE GIFTING', '3.0GB', '₦650.00', '30 days'],
       [73, '4', 'COOPERATE GIFTING', '5.0GB', '₦1,100.00', '30 days'],
-
-      [83, '1', 'GIFTING', '1.0GB', '₦275.00', '7 days'],
-      [85, '1', 'GIFTING', '3.0GB', '₦800.00', '1 Month'],
+      [43, '4', 'GIFTING', '40.0GB', '₦8,500.00', '1 Month'],
       ];
     </script>
 
@@ -195,31 +163,24 @@ if ($row) {
 
     $plansData = array(
       [6, '1', 'SME', '500MB', '₦145.00', '1 Month'],
+      [83, '1', 'GIFTING', '1.0GB', '₦250.00', '7 days'],
       [1, '1', 'SME', '1.0GB', '₦290.00', '1 Month'],
       [3, '1', 'SME', '2.0GB', '₦570.00', '1 Month'],
-      [2, '1', 'SME', '3.0GB', '₦850.00', '1 Month'],
+      [85, '1', 'GIFTING', '3.0GB', '₦800.00', '1 Month'],
       [4, '1', 'SME', '5.0GB', '₦1,400.00', '1 Month'],
       [5, '1', 'SME', '10.0GB', '₦2,750.00', '1 Month'],
-      
-      [7, '1', 'COOPERATE GIFTING', '500MB', '₦150.00', '1 Month'],
-      [8, '1', 'COOPERATE GIFTING', '1.0GB', '₦285.00', '1 Month'],
-      [9, '1', 'COOPERATE GIFTING', '2.0GB', '₦560.00', '1 Month'],
-      [10, '1', 'COOPERATE GIFTING', '3.0GB', '₦840.00', '1 Month'],
-      [11, '1', 'COOPERATE GIFTING', '5.0GB', '₦1,370.00', '1 Month'],
-      [12, '1', 'COOPERATE GIFTING', '10.0GB', '₦2,720.00', '1 Month'],
+      [51, '1', 'COOPERATE GIFTING', '15.0GB', '₦3,850.00', '30 days'],
 
+      [37, '2', 'COOPERATE GIFTING', '100MB', '₦60.00', '1 Month'],
+      [36, '2', 'COOPERATE GIFTING', '300MB', '₦100.00', '1 Month'],
       [13, '2', 'COOPERATE GIFTING', '500MB', '₦130.00', '1 Month'],
       [14, '2', 'COOPERATE GIFTING', '1.0GB', '₦240.00', '1 Month'],
       [15, '2', 'COOPERATE GIFTING', '2.0GB', '₦450.00', '1 Month'],
       [17, '2', 'COOPERATE GIFTING', '5.0GB', '₦1,070.00', '1 Month'],
       [18, '2', 'COOPERATE GIFTING', '10.0GB', '₦2,130.00', '1 Month'],
-      [36, '2', 'COOPERATE GIFTING', '300MB', '₦100.00', '1 Month'],
-      [37, '2', 'COOPERATE GIFTING', '100MB', '₦60.00', '1 Month'],
+      [62, '2', 'GIFTING', '20.0GB', '₦4,790.00', '30 days'],
 
-      [39, '3', 'GIFTING', '29.5GB', '₦7,500.00', '1 Month'],
-      [40, '3', 'GIFTING', '50.0GB', '₦9,500.00', '1 Month'],
-
-      [43, '4', 'GIFTING', '40.0GB', '₦8,500.00', '1 Month'],
+      
 
       [44, '3', 'COOPERATE GIFTING', '200MB', '₦105.00', '30 days'],
       [45, '3', 'COOPERATE GIFTING', '500MB', '₦140.00', '30 days'],
@@ -228,54 +189,38 @@ if ($row) {
       [48, '3', 'COOPERATE GIFTING', '3.0GB', '₦730.00', '30 days'],
       [49, '3', 'COOPERATE GIFTING', '5.0GB', '₦1,195.00', '30 days'],
       [50, '3', 'COOPERATE GIFTING', '10.0GB', '₦2,390.00', '30 days'],
-
-      [51, '1', 'COOPERATE GIFTING', '15.0GB', '₦3,850.00', '30 days'],
-
-      [53, '2', 'COOPERATE GIFTING', '20.0GB', '₦5,000.00', '30 days'],
-
-      [59, '1', 'GIFTING', '6.0GB', '₦2,420.00', '30 days'],
-      [60, '1', 'GIFTING', '10.0GB', '₦2,900.00', '30 days'],
-
-      [62, '2', 'GIFTING', '20.0GB', '₦5,000.00', '30 days'],
+      [39, '3', 'GIFTING', '29.5GB', '₦7,500.00', '1 Month'],
+      [40, '3', 'GIFTING', '50.0GB', '₦9,500.00', '1 Month'],
 
       [69, '4', 'COOPERATE GIFTING', '500MB', '₦140.00', '14 Days'],
       [70, '4', 'COOPERATE GIFTING', '1.0GB', '₦230.00', '30 days'],
       [71, '4', 'COOPERATE GIFTING', '2.0GB', '₦440.00', '30 days'],
       [72, '4', 'COOPERATE GIFTING', '3.0GB', '₦650.00', '30 days'],
       [73, '4', 'COOPERATE GIFTING', '5.0GB', '₦1,100.00', '30 days'],
-
-      [83, '1', 'GIFTING', '1.0GB', '₦275.00', '7 days'],
-      [85, '1', 'GIFTING', '3.0GB', '₦800.00', '1 Month'],
+      [43, '4', 'GIFTING', '40.0GB', '₦8,500.00', '1 Month'],
     );
     ?>
     <script>
       plansData = [
         [6, '1', 'SME', '500MB', '₦145.00', '1 Month'],
+      [83, '1', 'GIFTING', '1.0GB', '₦250.00', '7 days'],
       [1, '1', 'SME', '1.0GB', '₦290.00', '1 Month'],
       [3, '1', 'SME', '2.0GB', '₦570.00', '1 Month'],
-      [2, '1', 'SME', '3.0GB', '₦850.00', '1 Month'],
+      [85, '1', 'GIFTING', '3.0GB', '₦800.00', '1 Month'],
       [4, '1', 'SME', '5.0GB', '₦1,400.00', '1 Month'],
       [5, '1', 'SME', '10.0GB', '₦2,750.00', '1 Month'],
-      
-      [7, '1', 'COOPERATE GIFTING', '500MB', '₦150.00', '1 Month'],
-      [8, '1', 'COOPERATE GIFTING', '1.0GB', '₦285.00', '1 Month'],
-      [9, '1', 'COOPERATE GIFTING', '2.0GB', '₦560.00', '1 Month'],
-      [10, '1', 'COOPERATE GIFTING', '3.0GB', '₦840.00', '1 Month'],
-      [11, '1', 'COOPERATE GIFTING', '5.0GB', '₦1,370.00', '1 Month'],
-      [12, '1', 'COOPERATE GIFTING', '10.0GB', '₦2,720.00', '1 Month'],
+      [51, '1', 'COOPERATE GIFTING', '15.0GB', '₦3,850.00', '30 days'],
 
+      [37, '2', 'COOPERATE GIFTING', '100MB', '₦60.00', '1 Month'],
+      [36, '2', 'COOPERATE GIFTING', '300MB', '₦100.00', '1 Month'],
       [13, '2', 'COOPERATE GIFTING', '500MB', '₦130.00', '1 Month'],
       [14, '2', 'COOPERATE GIFTING', '1.0GB', '₦240.00', '1 Month'],
       [15, '2', 'COOPERATE GIFTING', '2.0GB', '₦450.00', '1 Month'],
       [17, '2', 'COOPERATE GIFTING', '5.0GB', '₦1,070.00', '1 Month'],
       [18, '2', 'COOPERATE GIFTING', '10.0GB', '₦2,130.00', '1 Month'],
-      [36, '2', 'COOPERATE GIFTING', '300MB', '₦100.00', '1 Month'],
-      [37, '2', 'COOPERATE GIFTING', '100MB', '₦60.00', '1 Month'],
+      [62, '2', 'GIFTING', '20.0GB', '₦4,790.00', '30 days'],
 
-      [39, '3', 'GIFTING', '29.5GB', '₦7,500.00', '1 Month'],
-      [40, '3', 'GIFTING', '50.0GB', '₦9,500.00', '1 Month'],
-
-      [43, '4', 'GIFTING', '40.0GB', '₦8,500.00', '1 Month'],
+      
 
       [44, '3', 'COOPERATE GIFTING', '200MB', '₦105.00', '30 days'],
       [45, '3', 'COOPERATE GIFTING', '500MB', '₦140.00', '30 days'],
@@ -284,24 +229,15 @@ if ($row) {
       [48, '3', 'COOPERATE GIFTING', '3.0GB', '₦730.00', '30 days'],
       [49, '3', 'COOPERATE GIFTING', '5.0GB', '₦1,195.00', '30 days'],
       [50, '3', 'COOPERATE GIFTING', '10.0GB', '₦2,390.00', '30 days'],
-
-      [51, '1', 'COOPERATE GIFTING', '15.0GB', '₦3,850.00', '30 days'],
-
-      [53, '2', 'COOPERATE GIFTING', '20.0GB', '₦5,000.00', '30 days'],
-
-      [59, '1', 'GIFTING', '6.0GB', '₦2,420.00', '30 days'],
-      [60, '1', 'GIFTING', '10.0GB', '₦2,900.00', '30 days'],
-
-      [62, '2', 'GIFTING', '20.0GB', '₦5,000.00', '30 days'],
+      [39, '3', 'GIFTING', '29.5GB', '₦7,500.00', '1 Month'],
+      [40, '3', 'GIFTING', '50.0GB', '₦9,500.00', '1 Month'],
 
       [69, '4', 'COOPERATE GIFTING', '500MB', '₦140.00', '14 Days'],
       [70, '4', 'COOPERATE GIFTING', '1.0GB', '₦230.00', '30 days'],
       [71, '4', 'COOPERATE GIFTING', '2.0GB', '₦440.00', '30 days'],
       [72, '4', 'COOPERATE GIFTING', '3.0GB', '₦650.00', '30 days'],
       [73, '4', 'COOPERATE GIFTING', '5.0GB', '₦1,100.00', '30 days'],
-
-      [83, '1', 'GIFTING', '1.0GB', '₦275.00', '7 days'],
-      [85, '1', 'GIFTING', '3.0GB', '₦800.00', '1 Month'],
+      [43, '4', 'GIFTING', '40.0GB', '₦8,500.00', '1 Month'],
       ];
     </script>
     <?php
