06e1dfc5685ebc96f473d0d50d629e7e53d4adab
Sir Abdulkarim Hussain SITD <abdulkarimhussain7@gmail.com>
2024-01-05 17:42:07 +0100

    index

--- dashboard/index/auth/index.php
+++ dashboard/index/auth/index.php
@@ -64,7 +64,7 @@ if (isset($_SESSION['email'])) {
 <head>
   <meta charset="utf-8" />
   <meta name="viewport"
-    content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    content="width=device-width, initial-scale=0.1, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
   <title>Login | ViraSub</title>
 
